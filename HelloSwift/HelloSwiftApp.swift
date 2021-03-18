1c592423c1155ae8d57e268750471391080d3a8d
Jean-Baptiste Meyer <meyer.jeanbaptiste@gmail.com>
2021-03-18 14:10:02 -0400

    first commit

---
+++ HelloSwift/HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftApp.swift
+//  HelloSwift
+//
+//  Created by Jean-Baptiste Meyer on 18/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
